9534c6a3f19a2b314ece2fcf81fc474c85733ebd
Orla Woods <orla.woods@gmail.com>
2025-12-05 20:37:17 +0000

    Review of Topic 2 task complete

--- topic02/assignment02-bankholidays.py
+++ topic02/assignment02-bankholidays.py
@@ -1,36 +1,40 @@
 # Thiis program prints out the bank holidays in Northern Ireland
 # Author: Orla Woods
 
+# Import mlibrary
 import requests  # requests library to handle http requests
+
+# Import data 
 url ="https://www.gov.uk/bank-holidays.json" 
 response = requests.get(url) # name the request function as response
 data = response.json() # define database using json
 
-# sanity check on the import
+# Sanity check on the import
 # print(data) 
 
-# using code from the lab. Print only data for Northern Ireland
+# Task 1: Print out the dates of Norther Ireland bank holidays 
 
-# print(data['northern-ireland']['events'])  
+# Using code from the lab
+print(f'Northern Ireland bank holidays are: {data['northern-ireland']['events']}')  
 
-# Last few marks: modify the program to print the bank holidays that are unique to Northern Ireland.
+# Task 2: modify the program to print the bank holidays that are unique to Northern Ireland.
 
-# using copilot code which is trying to look at the wrong part of the dataset, so modifying it, along
-# with code from the lab
+# Using copilot code which is trying to look at the wrong part of the dataset, so modifying it, 
+# along with code from the lab
 ni_holidays = data['northern-ireland']['events'] # define ni_holidays as the events in northern ireland
-# not from co-pilot but i knew i needed to define the events in england and wales and scotland
-# so i can say if "x is not in y"
+
+# I want to define the events in england and wales and scotland so i can say if "x is not in y"
 other_holidays = data['england-and-wales']['events'] + data['scotland']['events']
 
-# initial code was close but not there so i asked chatgpt for help:
+# Initial code was close but not there so i asked chatgpt for help:
 # https://chatgpt.com/share/68edffac-fbf4-800d-bc10-a69b4dbdd52e
-# I hadn't realised I should access the list of all other holidays first
-# access all other UK holiday titles for comparison
+
+# Access all other UK holiday titles for comparison
 other_titles = [holiday['title'] for holiday in other_holidays]
 
-# identify holidays unique to Northern Ireland
+# Identify holidays unique to Northern Ireland
 unique_ni_holidays = [holiday for holiday in ni_holidays if holiday['title'] not in other_titles]
 
-# print output
+# Print output
 for holiday in unique_ni_holidays:
     print(f"{holiday['title']} is a holiday unique to Northern Ireland and it takes place on {holiday['date']}") 
